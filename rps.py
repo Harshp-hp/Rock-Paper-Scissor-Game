d4b5185241dc22f0db08498eb16ece3fdd1b511e
Harshp-hp <72340908+Harshp-hp@users.noreply.github.com>
2020-10-12 08:12:00 +0530

    Create rps.py
    In the above program imported the random file for the computer. declare a set for the computer by the name of moves. so, the computer get the input.

---
+++ rps.py
@@ -0,0 +1,44 @@
+import random
+#moves for Computer
+moves = ["r","p", "s"]
+Keep_playing = "true"
+
+#while loop
+while Keep_playing == "true":
+    cmove = random.choice(moves)
+    pmove = input("Enter your choice: r, p or s?")
+
+    print("the computer choice : ", cmove)
+
+    if cmove == pmove:
+        print("Tie")
+    elif pmove == "r" and cmove == "p":
+        print("Computer won the game")
+    elif pmove == "r" and cmove == "s":
+        print("Player won the game")
+    elif pmove == "p" and cmove == "r":
+        print("Player won the game")
+    elif pmove == "s" and cmove == "p":
+        print("Player won the game")
+    elif pmove == "s" and cmove == "r":
+        print("Player won the game")
+    elif pmove == "p" and cmove == "s":
+        print("Player won the game")    
+        
+    else:
+        print("Please enter the valid inputs ['r','p','s']")
+
+        print("\n")
+        continue_input = input("Want to play again? (Y/N): ").casefold()
+        print("\n")
+
+        if(continue_input == 'y') or (continue_input == 'yes'):
+            continue
+        else:
+            print("see you again. Bye")
+            break
+  
+
+        
+        
+
